b01fd2201a1eadb8e783e653479a621f99ee8521
Hamza Bhatti <bhatti3993@gmail.com>
2024-12-07 13:04:37 +0500

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,7 +12,7 @@ function footer() {
     <>
       <div className="w-full bg-black text-[#FAFAFA] md:h-[400px]">
         <div className="flex flex-col md:flex-row flex-wrap">
-          <div className="w-full md:w-1/4	flex flex-col justify-center h-80  p-10">
+          <div className="w-full md:w-1/5	flex flex-col justify-center h-80  p-10">
             <h2 className="font-bold leading-6 text-2xl mt-6 md:mt-0">Exclusive</h2>
             <h3 className="font-medium leading-7 text-xl mt-6">Subscribe</h3>
             <p className=" mt-6">Get 10% off your first order</p>
@@ -27,7 +27,7 @@ function footer() {
               </Link>
             </div>
           </div>
-          <div className="w-full md:w-1/4	flex flex-col justify-center h-80  p-10">
+          <div className="w-full md:w-1/5	flex flex-col justify-center h-80  p-10">
             <h3 className="font-medium leading-7 text-xl">Support</h3>
             <p className="mt-6 text-base leading-6">
               111 Bijoy sarani, Dhaka, <br /> DH 1515, Bangladesh.
@@ -35,7 +35,7 @@ function footer() {
             <p className="mt-6">exclusive@gmail.com</p>
             <p className="mt-4">+88015-88888-9999</p>
           </div>
-          <div className="w-full md:w-1/4	flex flex-col text-center justify-center h-80  p-10">
+          <div className="w-full md:w-1/5	flex flex-col text-center justify-center h-80  p-10">
             <h3 className="font-medium leading-7 text-xl mt-14">Account</h3>
             <p className="mt-6">My Account</p>
             <p className="mt-6">Login / Register</p>
@@ -43,14 +43,14 @@ function footer() {
             <p className="mt-6">Wishlist</p>
             <p className="mt-6">Shop</p>
           </div>
-          <div className="w-full md:w-1/4	flex flex-col text-center justify-center h-80  p-10">
+          <div className="w-full md:w-1/5	flex flex-col text-center justify-center h-80  p-10">
             <h3 className="font-medium leading-7 text-xl mt-4">Quick Link</h3>
             <p className="mt-6">Privacy Policy</p>
             <p className="mt-6">Term of Use</p>
             <p className="mt-6">FAQ</p>
             <p className="mt-6">Contact</p>
           </div>
-          <div className="w-full md:w-1/4	flex flex-col justify-center h-80  p-6">
+          <div className="w-full md:w-1/5	flex flex-col justify-center h-80  p-6">
             <h3 className="font-medium leading-7 text-xl">Download App</h3>
             <p className="mt-6 leading-[18px] text-xs">
               Save $3 with App New User Only
